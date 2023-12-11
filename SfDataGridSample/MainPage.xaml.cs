d8ee6c5946985eeced4264823e0d0dc33a1215cc
Sudarsan Muthuselvan <sudarsan.m@syncfusion.com>
2023-12-11 18:49:53 +0530

    sample

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui;
+using Syncfusion.Maui.Data;
+using Syncfusion.Maui.DataGrid;
+using Syncfusion.Maui.DataGrid.Helper;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked()
+        {
+
+        }
+    }
+}
